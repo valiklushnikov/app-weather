7233746b59a356d8227529e75a7f631eccecb653
Valentyn <paladin567@gmail.com>
2025-08-09 16:57:15 +0200

    Add app domain

--- core/settings.py
+++ core/settings.py
@@ -17,8 +17,8 @@ if DEBUG:
     mimetypes.add_type("application/javascript", ".js", True)
     mimetypes.add_type("text/css", ".css", True)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = ["https://weather-app-services.up.railway.app",]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "app-weather-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://app-weather-production.up.railway.app",]
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
